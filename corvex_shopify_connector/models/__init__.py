b3a25ea7b6f305af4361c583c12f9308bd46df59
Mpho077 <69971504+Mpho077@users.noreply.github.com>
2026-03-19 18:41:00 +0200

    Add files via upload

---
+++ corvex_shopify_connector/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from . import shopify_instance
+from . import shopify_error_notification
+from . import shopify_price_sync
+from . import sale_order
+from . import stock_picking
+from . import res_config_settings
